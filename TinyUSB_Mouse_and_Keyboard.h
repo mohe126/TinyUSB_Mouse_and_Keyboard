f747f5294716a6a6dcf332d5056742869328ddda
mohe126 <mohamedbms2010@gmail.com>
2026-04-10 18:32:58 +0100

    Add pragma once directive for header guard

--- TinyUSB_Mouse_and_Keyboard.h
+++ TinyUSB_Mouse_and_Keyboard.h
@@ -40,6 +40,7 @@
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+#pragma once
 #ifndef USE_TINYUSB
   //if not using TinyUSB then default to the standard Arduino routines.
   #include <HID.h>
